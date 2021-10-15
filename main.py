ba0854f4e02224d6965ce383f92a08f684f8d9bf
vidddd <david@hipertintorero.com>
2021-10-15 19:28:52 +0200

    first

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+"""
+Module Docstring
+"""
+
+__author__ = "David Alvarez Calvo"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
